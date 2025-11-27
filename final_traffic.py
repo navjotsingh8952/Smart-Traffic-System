9fa79f989df4b8c6a6753a52ac8aaf90ee863a8a
navjotsingh8952 <navjotsingh8952@gmail.com>
2025-11-27 07:51:22 +0530

    minor change

--- final_traffic.py
+++ final_traffic.py
@@ -73,7 +73,7 @@ def get_counts_api():
     try:
         import requests
 
-        url = "https://projectmakerschn.in//api/get_values.php?id=38"
+        url = "https:/projectmakerschn.in/api/get_values.php?id=38"
 
         response = requests.request("GET", url)
 
